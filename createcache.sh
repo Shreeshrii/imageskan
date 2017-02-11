5b281859d1fcde3d22833e0cf3af22e27b8ce5b1
Shreeshrii <shreeshrii@gmail.com>
2017-02-11 11:22:53 +0530

    fix next run

--- createcache.sh
+++ createcache.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "cache already built, if running these commands, clear cache dirs first"
-
   wget -O ./jar/ocrevaluation.jar https://bintray.com/impactocr/maven/download_file?file_path=eu%2Fdigitisation%2FocrevalUAtion%2F1.3.0%2FocrevalUAtion-1.3.0-jar-with-dependencies.jar
 
 #   git submodule add https://github.com/Shreeshrii/imagessan
