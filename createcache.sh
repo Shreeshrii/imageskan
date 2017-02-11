3bc1d1cdad0b4d909e9e57a656ab2021abb45bb5
Shreeshrii <shreeshrii@gmail.com>
2017-02-11 08:53:32 +0530

    Update createcache.sh

--- createcache.sh
+++ createcache.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-   git submodule add https://github.com/Shreeshrii/imageshin
-   mv ./imagessan ./imagessan-save
-   
    git submodule add https://github.com/Shreeshrii/imagessan
-   mv ./imageshin ./imageshin-save
+   mv /home/travis/build/Shreeshrii/tess4eval/imagessan imagessan-save
+   
+   git submodule add https://github.com/Shreeshrii/imageshin
+   mv /home/travis/build/Shreeshrii/tess4eval/imageshin imageshin-save
    
    wget -O ./IIIT_Hindi_100.zip  http://ocr.iiit.ac.in/IIIT_Hindi_100.zip
    unzip ./IIIT_Hindi_100.zip
-   mv ./IIIT_Hindi_100 ./IIIT_Hindi_100-save
+   mv ./IIIT_Hindi_100 IIIT_Hindi_100-save
    
