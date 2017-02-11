d7ed97edcb7277f1af345bb17a7d46c94718755d
Shreeshrii <shreeshrii@gmail.com>
2017-02-11 10:00:41 +0530

    Update createcache.sh

--- createcache.sh
+++ createcache.sh
@@ -2,6 +2,10 @@
 
 echo "cache already built, if running these commands, clear cache dirs first"
 
+mkdir jar
+wget -O ./jar/ocrevaluation.jar https://bintray.com/impactocr/maven/download_file?file_path=eu%2Fdigitisation%2FocrevalUAtion%2F1.3.0%2FocrevalUAtion-1.3.0-jar-with-dependencies.jar
+
+
 #   git submodule add https://github.com/Shreeshrii/imagessan
 #   mv /home/travis/build/Shreeshrii/tess4eval/imagessan imagessan-save
 #   ls imagessan-save
