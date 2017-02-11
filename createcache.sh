cf07e1b5286670b7c82299572b2b298d5992382a
Shreeshrii <shreeshrii@gmail.com>
2017-02-11 10:03:58 +0530

    Update createcache.sh

--- createcache.sh
+++ createcache.sh
@@ -2,7 +2,6 @@
 
 echo "cache already built, if running these commands, clear cache dirs first"
 
-mkdir jar
 wget -O ./jar/ocrevaluation.jar https://bintray.com/impactocr/maven/download_file?file_path=eu%2Fdigitisation%2FocrevalUAtion%2F1.3.0%2FocrevalUAtion-1.3.0-jar-with-dependencies.jar
 
 
