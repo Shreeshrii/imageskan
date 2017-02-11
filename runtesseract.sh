b1e678e0e534a8857bb580cdf1ad5530a7f863d0
Shreeshrii <shreeshrii@gmail.com>
2017-02-11 11:35:42 +0530

    Update runtesseract.sh

--- runtesseract.sh
+++ runtesseract.sh
@@ -7,6 +7,6 @@ cd ${TRAVIS_BUILD_DIR}/tif
     for img_file in ${img_files}; do
         filename=$(basename "${img_file##*/}" .tif)
         echo ${img_file}
-        tesseract ${img_file} ${TRAVIS_BUILD_DIR}/ocr/${filename} --psm 6 --oem 1 -l hin 
+        tesseract ${img_file} ${TRAVIS_BUILD_DIR}/ocr/${filename} --psm 6 --oem 1 -l kan 
     done   
   
