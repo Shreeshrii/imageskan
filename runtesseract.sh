d64d55789aefa8fd9bb464bf05723d97d4ae67b7
Shreeshrii <shreeshrii@gmail.com>
2017-02-11 11:15:38 +0530

    Update runtesseract.sh

--- runtesseract.sh
+++ runtesseract.sh
@@ -1,54 +1,12 @@
 #!/bin/bash
 
-echo "using saved cache"
-echo "Images from Shreeshrii/imageshin"
+echo " images from tif"
 
-cd ${TRAVIS_BUILD_DIR}/imageshin-save/imageshin
-    img_files=$(ls *.png)
-    for img_file in ${img_files}; do
-        filename=$(basename "${img_file##*/}" .png)
-        echo ${img_file}
-        cp ${filename}.txt ${TRAVIS_BUILD_DIR}/gt
-        tesseract ${img_file} ${TRAVIS_BUILD_DIR}/ocr/${filename} --psm 6 --oem 1 -l hin 
-    done   
-    
- cd ${TRAVIS_BUILD_DIR}/IIIT_Hindi_100-save/IIIT_Hindi_100
- echo "Images from http://ocr.iiit.ac.in/Hindi100.html"
- cd Images
-     img_files=$(ls *.jpg)
-    for img_file in ${img_files}; do
-        filename=$(basename "${img_file##*/}" .jpg)
-        echo ${img_file}
-        cp ../GT/${filename}.txt ${TRAVIS_BUILD_DIR}/gt
-        tesseract  ${img_file}  ${TRAVIS_BUILD_DIR}/ocr/${filename} --psm 6 --oem 1 -l san 
-    done   
-       
- cd ${TRAVIS_BUILD_DIR}/imagessan-save/imagessan
- echo "Images from Shreeshrii/imagessan/groundtruthimages"
- cd groundtruthimages
-     img_files=$(ls *.png)
-    for img_file in ${img_files}; do
-        filename=$(basename "${img_file##*/}" .png)
-        echo ${img_file}
-        cp ${filename}.txt ${TRAVIS_BUILD_DIR}/gt
-        tesseract  ${img_file}  ${TRAVIS_BUILD_DIR}/ocr/${filename} --psm 6 --oem 1 -l san 
-    done   
-    
- cd ../oldstylefontsamples
-  echo "Images from Shreeshrii/imagessan/oldstylefontsamples"
-    img_files=$(ls *.png)
-    for img_file in ${img_files}; do
-        filename=$(basename "${img_file##*/}" .png)
-        echo ${img_file}
-        cp ${filename}.txt ${TRAVIS_BUILD_DIR}/gt
-        tesseract  ${img_file}  ${TRAVIS_BUILD_DIR}/ocr/${filename} --psm 6 --oem 1 -l san 
-    done       
-    
+cd ${TRAVIS_BUILD_DIR}/tif
     img_files=$(ls *.tif)
     for img_file in ${img_files}; do
         filename=$(basename "${img_file##*/}" .tif)
         echo ${img_file}
-        cp ${filename}.txt ${TRAVIS_BUILD_DIR}/gt
-        tesseract  ${img_file} ${TRAVIS_BUILD_DIR}/ocr/${filename} --psm 6 --oem 1 -l san 
-    done 
-    
+        tesseract ${img_file} ${TRAVIS_BUILD_DIR}/ocr/${filename} --psm 6 --oem 1 -l hin 
+    done   
+  
