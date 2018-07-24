b81d4989e39b7444ee140ed1397c7f491239dfd8
Shreeshrii <shreeshrii@gmail.com>
2018-07-24 22:55:31 +0530

    Create createcache.sh

---
+++ createcache.sh
@@ -0,0 +1,8 @@
+mkdir tesseract-ocr
+cd tesseract-ocr
+git clone --depth 1 https://github.com/tesseract-ocr/tesseract.git
+git clone --depth 1 https://github.com/tesseract-ocr/langdata.git
+cd tesseract/tessdata
+wget https://raw.githubusercontent.com/tesseract-ocr/tessdata_best/master/eng.traineddata
+wget https://raw.githubusercontent.com/tesseract-ocr/tessdata_best/master/osd.traineddata
+ls -l
