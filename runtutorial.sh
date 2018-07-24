2c4c3aa3acc1f5181038177683e7033b07aa5618
Shreeshrii <shreeshrii@gmail.com>
2018-07-24 22:55:51 +0530

    Update runtutorial.sh

--- runtutorial.sh
+++ runtutorial.sh
@@ -1,8 +1,1 @@
-mkdir tesseract-ocr
-cd tesseract-ocr
-git clone --depth 1 https://github.com/tesseract-ocr/tesseract.git
-git clone --depth 1 https://github.com/tesseract-ocr/langdata.git
-cd tesseract/tessdata
-wget https://raw.githubusercontent.com/tesseract-ocr/tessdata_best/master/eng.traineddata
-wget https://raw.githubusercontent.com/tesseract-ocr/tessdata_best/master/osd.traineddata
-ls -l
+echo "running tutorial"
